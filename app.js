18feb674a8eee6556d7b230926746e0205e1fe17
Aryainguz <aryaninguz369@gmail.com>
2024-11-05 14:11:43 +0530

    feat: added api health check route

--- app.js
+++ app.js
@@ -11,5 +11,9 @@ app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/post', postRoutes);
 app.use('/api/v1/user', followRoutes);
 
+app.get("/",(req,res)=>{
+    res.send("API Healthy!")
+})
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
